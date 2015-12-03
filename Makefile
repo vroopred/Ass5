55d07285f4fe2eeff9d70e2182da209aafe4cffc
Varsha Roopreddy <vroopred@calpoly.edu>
2015-12-03 00:06:58 -0800

    Break up file to lib files for reuse
    Issues with minget not getting the file inode when searchZone is called

--- Makefile
+++ Makefile
@@ -1,15 +1,22 @@
 CC = gcc
 CFLAGS = -Wall -g
 
-all: clean minls 
+all: clean minls minget
 
-minls: minls.o 
-	$(CC) $(CFLAGS) -o minls minls.o 
+minls: minls.o lib.o
+	$(CC) $(CFLAGS) -o minls minls.o lib.o 
 
+minget: minget.o lib.o
+	$(CC) $(CFLAGS) -o minget minget.o lib.o
+
+lib.o: 
+	$(CC) $(CFLAGS) -c -o lib.o lib.c	
 
 minls.o: 
 	$(CC) $(CFLAGS) -c -o minls.o minls.c
 
+minget.o:
+	$(CC) $(CFLAGS) -c -o minget.o minget.c
 
 clean:
 	rm -rf *.o minls minget 
--- minget.c
+++ minget.c
@@ -3,107 +3,191 @@
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
-#include "minls.h"
+#include <getopt.h>
+#include "lib.h"
 
-filesystem stuff;
-filesystem *fileSys;
-int verbose = 0;
+void copy();
+
+extern filesystem fileSys;
+extern int verbose;
+FILE *dstpath;
 
 int main(int argc, char *argv[]) {
    /*minget [ -v ] [ -p part [ -s subpart ] ] imagefile srcpath [ dstpath ]
     Minget copies a regular file from the given source path to the given destination
     path. If the destination path is ommitted, it copies to stdout*/
    
-   int i = 1;
-   int partition = 0;
-   int subpartition = 0;
+   extern char *optarg;
+   extern int optind;
+   FILE* dstpath = NULL;
    char* imagefile = 0;
-   char* path = 0;
-   char* srcpath;
-   char* dstpath;
-   
-   fileSys = &stuff;
-   
-   if(argc == 1 || (argv[i][0] == '-' && argv[i][1] == 'h')) {
-      printf(
-             "usage: minget [ -v ] [ -p part [ -s subpart ] ] imagefile srcpath [ dstpath ]\n"
-             "Options:\n"
-             "-p part    --- select partition for filesystem (default: none)\n"
-             "-s sub     --- select subpartition for filesystem (default: none)\n"
-             "-h help    --- print usage information and exit\n"
-             "-v verbose --- increase verbosity level\n"
-             );
-      return 0;
-   }
-   
-   if(argv[i][0] == '-' && argv[i][1] == 'v') {
-      printf("Verbose\n");
-      verbose = 1;
-      i++;
-   }
-   
-   if(i < argc && argv[i][0] == '-' && argv[i][1] == 'p') {
-      if(i + 2 > argc) {
-         fprintf(stderr, "Not enough arguments for %s flag\n", argv[i]);
-         return -1;
+   int c = getopt(argc, argv, "vp:s:h");
+   int sflag = 0, pflag = 0;
+   int isPart = 0;
+   verbose = 0;
+   fileSys.part = -1;
+   fileSys.subPart = -1;
+   fileSys.path = 0;
+   while (c != -1) {
+      switch (c) {
+         case 'v':
+            verbose = 1;
+            break;
+         case 'p':
+            isPart = 1;
+            pflag = 1;
+            fileSys.part = atoi(optarg);
+            break;
+         case 's':
+            sflag = 1;
+            fileSys.subPart = atoi(optarg);
+            break;
+         case 'h':
+            fprintf(stderr,
+                    "usage: minget [ -v ] [ -p part [ -s subpart ] ] imagefile srcpath [ dstpath ]\n"
+                    "Options:\n"
+                    "-p part    --- select partition for filesystem (default: none)\n"
+                    "-s sub     --- select subpartition for filesystem (default: none)\n"
+                    "-h help    --- print usage information and exit\n"
+                    "-v verbose --- increase verbosity level\n"
+                    );
+            exit(EXIT_FAILURE);
+         default:
+            fprintf(stderr,
+                    "usage: minget [ -v ] [ -p part [ -s subpart ] ] imagefile srcpath [ dstpath ]\n"
+                    "Options:\n"
+                    "-p part    --- select partition for filesystem (default: none)\n"
+                    "-s sub     --- select subpartition for filesystem (default: none)\n"
+                    "-h help    --- print usage information and exit\n"
+                    "-v verbose --- increase verbosity level\n"
+                    );
+            exit(EXIT_FAILURE);
+            
       }
-      i++;
-      partition = atoi(argv[i++]);
-      fileSys->part = partition;
-      printf("Partition: %d\n", partition);
-      if(i < argc && argv[i][0] == '-' && argv[i][1] == 's') {
-         printf("Getting here\n");
-         if(i + 2 > argc) {
-            fprintf(stderr, "Not enough arguments for %s flag\n", argv[i]);
-            return -1;
-         }
-         i++;
-         subpartition = atoi(argv[i++]);
-         fileSys->subPart = subpartition;
-         printf("Subpartition: %d\n", subpartition);
+      if (pflag == 1 && ((optind+1) > argc)) {
+         fprintf(stderr, "Not enough arguments for -p flag\n");
+         exit(EXIT_FAILURE);
       }
-      else {
-         fileSys->subPart = -1;
+      if (sflag == 1 && isPart == 0) {
+         fprintf(stderr, "Cannot have subpartition without partition.\n");
+         exit(EXIT_FAILURE);
+      }
+      else if (sflag == 1 && ((optind+1) > argc)) {
+         fprintf(stderr, "Not enough arguments for -s flag\n");
+         exit(EXIT_FAILURE);
+      }
+      pflag = sflag = 0;
+      c = getopt(argc, argv, "vp:s:h");
+   }
+   if (optind >= argc) {
+      fprintf(stderr, "Need an imagefile.\n");
+      exit(EXIT_FAILURE);
+   }
+   else {
+      fileSys.imageFile = fopen(argv[optind++], "rb");
+      if (fileSys.imageFile == NULL) {
+         fprintf(stderr, "Cannot open the image file %s\n", imagefile);
+         exit(EXIT_FAILURE);
+      }
+   }
+   if (optind >= argc) {
+      fprintf(stderr, "Need an srcpath.\n");
+      exit(EXIT_FAILURE);
+   }
+   else {
+      fileSys.path = argv[optind++];
+      printf("%s\n",fileSys.path);
+   }
+   if (optind < argc) {
+      if ((dstpath = fopen(argv[optind++], "w+")) == NULL) {
+         fprintf(stderr, "Cannot write to dstfile.\n");
+         exit(EXIT_FAILURE);
       }
    }
    else {
-      fileSys->part = -1;
+      dstpath = stdout;
    }
    
-   if(i < argc) {
-      imagefile = argv[i++];
-      /*open file image and set the FILE pointer to the field*/
-      fileSys->imageFile = fopen(imagefile, "rb");
-      if (fileSys->imageFile == NULL) {
-         fprintf(stderr, "Cannot open the image file %s\n", imagefile);
-      }
-      srcpath = argv[++];
-   }
-   
-   if(i < argc) {
-      dstpath = argv[i++];
-      //fileSys->path = path;
-      fprintf(stderr, "Path is %s\n", path);
-   }
    /*Set the bootblock*/
-   fileSys->bootblock = 0;
+   fileSys.bootblock = 0;
    /*Partition => call findPartition() for part && subpart (disk starts at first
     sector of part for the subpart but everything else is the same)
     Superblock
     get inode
     print listing
     close file*/
-   if (fileSys->part > -1) {
-      findPartition(fileSys->part);
-      if (fileSys->subPart > -1) {
-         findPartition(fileSys->subPart);
+   if (fileSys.part > -1) {
+      findPartition(fileSys.part);
+      if (fileSys.subPart > -1) {
+         findPartition(fileSys.subPart);
       }
    }
    findSuperBlock();
+   copy();
    return 0;
 }
 
+   /*1. Using findPath, get the inode of the file from that path
+     2. Check if its not a directory
+    3. Get the contents of the file
+    */
+
 void copy() {
-   //open file
-   //regular file check
+   inode node;
+   printNode newNode;
+   int i;
+   void * buffer;
+   int32_t nodeSize;
+   int32_t readSize;
+   
+   buffer = malloc(fileSys.zonesize);
+   fseek(fileSys.imageFile, fileSys.bootblock + fileSys.blocksize +
+         fileSys.blocksize + fileSys.blocksize * fileSys.super.i_blocks +
+         fileSys.blocksize * fileSys.super.z_blocks, SEEK_SET);
+   fread(&node, sizeof(inode), 1, fileSys.imageFile);
+   
+   newNode = searchZone(node);
+   nodeSize = newNode.size;
+   
+   if(!newNode.found) {
+      fprintf(stderr, "File not found.\n");
+      exit(EXIT_FAILURE);
+   }
+
+   //Check if it's a file. This is special and gets printed differently
+   if(!IS_DIRECTORY(newNode.mode)) {
+      fprintf(stderr, "Not a regular file.\n");
+      exit(EXIT_FAILURE);
+   }
+   
+   for (i = 0; i < DIRECT_ZONES; i++) {
+      if(nodeSize < 0) {
+         if (nodeSize < fileSys.zonesize) {
+            readSize = nodeSize;
+         }
+         else {
+            readSize = fileSys.zonesize;
+         }
+         if (!newNode.zone[i]) {
+            memset(buffer, 0, readSize);
+         }
+         else {
+            fseek(fileSys.imageFile,
+               fileSys.bootblock + (fileSys.zonesize * newNode.zone[i]), SEEK_SET);
+            fread(buffer, readSize, 1, fileSys.imageFile);
+         }
+         nodeSize -= readSize;
+         fwrite(buffer, readSize, 1, dstpath);
+      }
+   }
+   
 }
+
+
+
+
+
+
+
+
+
